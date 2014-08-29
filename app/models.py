9a38b67113108e375693a3ace4759a13d2f44cb0
zhengnan <zhengnan1987@gmail.com>
2014-08-29 09:20:14 -0700

    Long time no seehistory | grep commit!

--- app/models.py
+++ app/models.py
@@ -3,15 +3,15 @@ from app import db
 ROLE_USER=0
 ROLE_ADMIN=1
 
-class User(db.Model):
-    id=db.Column(db.Integer, primary_key=True)
-    nickname=db.Column(db.String(64),  unique=True)
-    email=db.Column(db.String(120), unique=True)
-    role=db.Column(db.SmallInteger, default=ROLE_USER)
-    posts=db.relationship('Post', backref='author', lazy='dynamic')
+#class User(db.Model):
+#    id=db.Column(db.Integer, primary_key=True)
+#    nickname=db.Column(db.String(64),  unique=True)
+#    email=db.Column(db.String(120), unique=True)
+#    role=db.Column(db.SmallInteger, default=ROLE_USER)
+#    posts=db.relationship('Post', backref='author', lazy='dynamic')
 
-    def __repr__(self):
-        return '<User %r>' % (self.nickname)
+#    def __repr__(self):
+#        return '<User %r>' % (self.nickname)
 
 class Post(db.Model):
     id=db.Column(db.Integer, primary_key=True)
@@ -21,3 +21,25 @@ class Post(db.Model):
 
     def __repr__(self):
         return '<Post %r>' % (self.body)
+
+class User(db.Model):
+    id=db.Column(db.Integer, primary_key=True)
+    nickname=db.Column(db.String(64), unique=True)
+    email=db.Column(db.String(120), unique=True)
+    role=db.Column(db.SmallInteger, default=ROLE_USER)
+    posts=db.relationship('Post', backref='author', lazy='dynamic')
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
+
+    def __repr__(self):
+        return '<User %r>' % (self.nickname)
--- app/views.py
+++ app/views.py
@@ -1,11 +1,14 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from app import app, db, lm, oid
 from forms import LoginForm
+from flask.ext.login import login_user, logout_user, current_user, login_required
+from models import User, ROLE_USER, ROLE_ADMIN
 
 @app.route('/')
 @app.route('/index')
+@login_required
 def index():
-    user={'nickname':'Focus'}
+    user=g.user
     posts=[
         {
             'author':{'nickname':'John'},
@@ -23,13 +26,51 @@ def index():
                            posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
+@oid.loginhandler
 def login():
+    if g.user is not None and g.user.is_authenticated():
+        return redirect(url_for('index'))
     form=LoginForm()
     if form.validate_on_submit():
-        flash('Login requested for OpenID="'+form.openid.data+'", remember_me='
-              +str(form.remember_me.data))
-        return redirect('/index')
+        session['remember_me']=form.remember_me.data
+#        flash('Login requested for OpenID="'+form.openid.data+'", remember_me='
+#              +str(form.remember_me.data))
+#        return redirect('/index')
+        return oid.try_login(form.openid.data, ask_for=['nickname','email'])
     return render_template('login.html',
                            title='Sign In',
                            form=form,
                            providers=app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+    if resp.email is None or resp.email=="":
+        flash('Invalid login.Please try again.')
+        return redirect(url_for('login'))
+    user=User.query.filter_by(email=resp.email).first()
+    if user is None:
+        nickname=resp.nickname
+        if nickname is None or nickname=="":
+            nickname=resp.email.split('@')[0]
+        user=User(nickname=nickname, email=resp.email, role=ROLE_USER)
+        db.session.add(user)
+        db.session.commit()
+    remember_me-False
+    if 'remember_me' in session:
+        remember_me=session['remember_me']
+        session.pop('remember_me', None)
+    login_user(user, remember=remember_me)
+    return rediect(request.args.get('next') or url_for('index'))
+
+@lm.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+@app.before_request
+def before_request():
+    g.user=current_user
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
